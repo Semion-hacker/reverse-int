0fcafa36fcf9f6d1eac93ab7fd3fcd3a78b8b6c9
Semion-hacker <obucheniekurs7@gmail.com>
2023-08-16 23:26:33 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+  const reverse = (s) => s.split('').reverse().join('');
+  if (n > 0) {
+    const result = n.toString();
+    const result2 = reverse(result);
+    return parseInt(result2);
+  } else if (n < 0) {
+    n = n * -1;
+    const result3 = n.toString();
+    const result4 = reverse(result3);
+    return parseInt(result4);
+  } else {
+    return 0;
+  };
+};
